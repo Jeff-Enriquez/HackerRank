16d740eb2e2b5fcf856c4a32f4c76c9f896180b6
Jeff Enriquez <jeffenriquez@Jeffs-MBP.home>
2021-07-16 18:06:45 -0700

    Improve SpecialStringAgain

--- interviewpreparationkit/stringmanipulation/SpecialStringAgain.java
+++ interviewpreparationkit/stringmanipulation/SpecialStringAgain.java
@@ -22,64 +22,38 @@ public class SpecialStringAgain {
     }   
 
     // Big O Space Complexity: O(1)
-    // Big O Time Complexity: O(n^2)
+    // Big O Time Complexity: O(n)
     public static int solution1(String specialString){
+        
+        // Keep track fo count
+        int count = 0;
 
-        // The loop doesn't count the last character so we start with 1
-        int count = 1;
-
-        // Loop through the string
+        // Loop through every letter
         int i = 0;
-        while(i < specialString.length()-1){
+        while(i < specialString.length()){
+            int repeat = 1;
+            // If the letters match increment repeat count
+            while(i < specialString.length()-1 && specialString.charAt(i) == specialString.charAt(i+1)){
+                repeat++;
+                i++;
+            }
+            i++;
 
-            // Keep track of the main character
-            char mainChar = specialString.charAt(i);
+            // Count the combinations of the repeating letters
+            count += (repeat * repeat + repeat) / 2;
 
-            //Track number of characters left of the exception
-            int leftCount = 1;
-
-            // Loop through the rest of the string until it is no longer a special string
-            int j = i;
-            while(j < specialString.length()-1){
-
-                // If the main character equals the secondary character: increase the count
-                if(mainChar == specialString.charAt(++j)){
-                    // Add to left count
-                    leftCount++;
-                    // If we are at the end of the string set i = j to end both loops.
-                    if(j == specialString.length()){
-                        i = j;
-                    }
-                } 
-                // If the characters do not match we have hit our exception 
-                else{
-                    // Keep track of how many matching characters there are to the right of the exception
-                    int rightCount = 0;
-                    // We will start the next iteration at the exception because we know the count to the left of it
-                    i = j;
-                    final int LEFT_COUNT = leftCount;
-                    while(leftCount-- > 0 && j < specialString.length()-1 && mainChar == specialString.charAt(++j)){
-                        rightCount++;
-                    }
-                    count += getCount(LEFT_COUNT, rightCount);
-                    break;
-                }
+            int pointer = 1;
+            // Check for all special combinations
+            while(i-pointer >= 0 && i+pointer < specialString.length() 
+            && specialString.charAt(i-1) == specialString.charAt(i+pointer)
+            && specialString.charAt(i-1) == specialString.charAt(i-pointer)){
+                count++;
+                pointer++;
             }
         }
+
         return count;
     }
-
-    // This will give the total combinations of the leftside and add the count of the right side
-    private static int getCount(int leftCount, int rightCount){
-        int totalCount = 0;
-        int sumOfLeft = 0;
-        for(int i = 1; i <= leftCount; i++){
-            sumOfLeft += i;
-        }
-        totalCount = sumOfLeft + rightCount;
-        return totalCount;
-    }
-    
 
     private static void printTest(String solution, long result, long expected){
         System.out.print( "\u001B[33m" +
